4e2f61ee2bd329cfada738f8ab2e415eafbc8989
brolesi <brolesi@gmail.com>
2014-01-19 11:31:12 -0200

    README.MD
    DEMO: http://fabio.mat.br/ambiente/proporcionalidade_e_rateio_de_valores

--- include-header.php
+++ include-header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- "Se eu tenho uma maçã e você tem uma maçã, e nós as trocarmos, cada um continua com uma maçã. Se eu tenho uma idéia e você tem uma idéia, e nós as trocarmos, cada um fica com duas idéias." George Bernard Shaw - Dramatrugo Irlandês -->
     <title>Proporcionalidade e rateio de valores</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8">
---
+++ readme.md
@@ -0,0 +1,6 @@
+Proporcionalidade e rateio de valores
+=====================================
+
+"Se eu tenho uma maçã e você tem uma maçã, e nós as trocarmos, cada um continua com uma maçã. Se eu tenho uma idéia e você tem uma idéia, e nós as trocarmos, cada um fica com duas idéias." George Bernard Shaw - Dramatrugo Irlandês
+
+Demo: http://fabio.mat.br/ambiente/proporcionalidade_e_rateio_de_valores
